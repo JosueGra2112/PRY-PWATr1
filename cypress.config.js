e3b2ac53acac94ec493865c1adbb7a96ab1cbd5c
Josue Granados <20211306@uthh.edu.mx>
2024-10-24 12:22:39 -0600

    Implementacion de cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     // Configura la ruta base para las pruebas
-    baseUrl: 'http://localhost:3000', // Cambia esto según sea necesario
+    baseUrl: 'http://localhost:3000/', // Cambia esto según sea necesario
     supportFile: 'cypress/support/index.js', // Asegúrate de que este archivo exista
   },
 });
