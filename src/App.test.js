0afafe4cfed531ec6ac78a7a6f0b9e2edecd70c5
Josue Granados <20211306@uthh.edu.mx>
2024-10-24 10:30:25 -0600

    Configuracion de Service-Worker

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
-import 'matchmedia-polyfill'; // Asegúrate de que esto esté al principio
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import messaging from './firebase'; // Asegúrate de que esta ruta sea correcta
+
+jest.mock('./firebase', () => ({
+  messaging: {}, // Mockea el objeto messaging
+}));
 
 test('renders learn react link', () => {
   render(<App />);
