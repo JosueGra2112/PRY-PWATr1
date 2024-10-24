dd933141ed5be755521d1b2577f3219c9611a577
Josue Granados <20211306@uthh.edu.mx>
2024-10-24 12:29:14 -0600

    Implementacion de cypress.config Act

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 // cypress.config.js
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     // Configura la ruta base para las pruebas
-    baseUrl: 'http://localhost:3000/', // Cambia esto según sea necesario
+    baseUrl: 'http://localhost:3000',
+    requestTimeout: 30000, // 30 segundos
+    responseTimeout: 30000, // 30 segundos
     supportFile: 'cypress/support/index.js', // Asegúrate de que este archivo exista
   },
 });
