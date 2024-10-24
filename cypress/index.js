a34f5e8161ab09e0eb2bb0fdb62cd48f91aab5aa
Josue Granados <20211306@uthh.edu.mx>
2024-10-24 12:09:39 -0600

    Implementacion de cypress

--- cypress/index.js
+++
@@ -1,6 +0,0 @@
-// cypress/support/index.js
-
-// Aquí puedes incluir configuraciones globales para tus pruebas.
-Cypress.Commands.add('login', (username, password) => {
-    // Lógica para el inicio de sesión
-});
