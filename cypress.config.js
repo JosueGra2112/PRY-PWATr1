152f3677e5451afe78f87511bc75208b87bebd68
Josue Granados <20211306@uthh.edu.mx>
2024-10-24 11:42:30 -0600

    Implementacion de cypress

---
+++ cypress.config.js
@@ -0,0 +1,7 @@
+module.exports = defineConfig({
+    e2e: {
+      supportFile: false, // Desactiva el archivo de soporte si no es necesario
+      baseUrl: 'http://localhost:3000', // Cambia esto según sea necesario
+    },
+  });
+  
---
+++ cypress/index.js
@@ -0,0 +1,6 @@
+// cypress/support/index.js
+
+// Aquí puedes incluir configuraciones globales para tus pruebas.
+Cypress.Commands.add('login', (username, password) => {
+    // Lógica para el inicio de sesión
+});
