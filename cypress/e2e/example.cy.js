df456f7d5149e88e666d0677362ba80b4d46ec05
Josue Granados <20211306@uthh.edu.mx>
2024-10-27 13:17:04 -0600

    Correcion en el yml}

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -1,21 +1,21 @@
 // cypress/e2e/example.cy.js
 describe('Pruebas de la Aplicación', () => {
-    it('Visita la página de inicio y verifica el título', () => {
+    it('Visita la página de inicio y verifica el contenido', () => {
         cy.visit('http://localhost:3000'); // Asegúrate de que este sea el correcto
         cy.contains('SISTEMA INTEGRAL DE GESTION ADMINISTRATIVA'); // Verifica que el título esté presente
     });
 
-    it('Visita la página de Calendario y verifica el título', () => {
+    it('Visita la página de Calendario y verifica el contenid', () => {
         cy.visit('http://localhost:3000/#/Calendario'); // Asegúrate de que este sea el correcto
         cy.contains('Calendarios escolares'); // Verifica que el título esté presente
     });
 
-    it('Visita la página de Bitácora y verifica el título', () => {
+    it('Visita la página de Bitácora y verifica los datos', () => {
         cy.visit('http://localhost:3000/#/Bitacoras'); // Asegúrate de que este sea el correcto
         cy.contains('BITÁCORA'); // Verifica que el título esté presente
     });
 
-    it('Visita la página de Boletín y verifica el título', () => {
+    it('Visita la página de Boletín y verifica el los datos', () => {
         cy.visit('http://localhost:3000/#/Boletin'); // Asegúrate de que este sea el correcto
         cy.contains('BOLETÍN'); // Verifica que el título esté presente
     });
